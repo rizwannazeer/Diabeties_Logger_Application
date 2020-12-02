3b5570e8e2b2d8ee32dd8398b2e4fbe3c7d8d61e
rizwannazeer <71546360+rizwannazeer@users.noreply.github.com>
2020-12-02 07:46:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -17,11 +17,7 @@ app.use(cors())
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*',(req, res)=>{
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -33,7 +29,11 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/records',recordsRouter);
 app.use('/api/patients',patientsRouter);
+app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.get('*',(req, res)=>{
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
